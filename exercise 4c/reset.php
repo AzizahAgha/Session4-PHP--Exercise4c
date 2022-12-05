80e6b9414c91575d7d929913b4ffa00f7acce4ba
Azizah Agha <117432933+AzizahAgha@users.noreply.github.com>
2022-12-05 10:07:11 +0300

    Update reset.php

--- exercise 4c/reset.php
+++ exercise 4c/reset.php
@@ -28,29 +28,29 @@ if(isset($_POST['submit'])){
         $email = trim($_POST["email"]);
     }
 
-    if(empty(trim($_POST["password"]))){
+   if(empty(trim($_POST["password"]))){
         $password_err = "Please enter the OLD password.";     
-    } elseif(strlen(trim($_POST["password"])) < 6){
+    } elseif(strlen(md5($_POST['password'])) < 6){
         $password_err = "Password must have atleast 6 characters.";
     } else{
-        $password = trim($_POST["password"]);
+        $password = md5($_POST['password']);
     }
     
    
 
     if(empty(trim($_POST["new_password"]))){
         $new_password_err = "Please enter the new password.";     
-    } elseif(strlen(trim($_POST["new_password"])) < 6){
+    } elseif(strlen(md5($_POST["new_password"])) < 6){
         $new_password_err = "Password must have atleast 6 characters.";
     } else{
-        $new_password = trim($_POST["new_password"]);
+        $new_password = md5($_POST["new_password"]);
     }
     
     // Validate confirm password
     if(empty(trim($_POST["confirm_password"]))){
         $confirm_password_err = "Please confirm the password.";
     } else{
-        $confirm_password = trim($_POST["confirm_password"]);
+        $confirm_password = md5($_POST["confirm_password"]);
         if(empty($new_password_err) && ($new_password != $confirm_password)){
             $confirm_password_err = "Password did not match.";
         }
